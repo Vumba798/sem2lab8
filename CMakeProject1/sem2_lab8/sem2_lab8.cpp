f170c8092d8e600a120368c29a7e7a8abd5f36eb
Vumba798 <61025343+Vumba798@users.noreply.github.com>
2020-05-20 17:44:32 +0300

    new

---
+++ CMakeProject1/sem2_lab8/sem2_lab8.cpp
@@ -0,0 +1,77 @@
+﻿// sem2_lab8.cpp: определяет точку входа для приложения.
+//
+#include "sem2_lab8.h"
+
+using std::endl;
+using std::cin;
+using std::cout;
+
+
+std::vector<int> maxNum;
+
+std::mutex m;
+
+void findMax(std::vector<int>::iterator it1, std::vector<int>::iterator it2) {
+
+	int value = *std::max_element(it1, it2);
+	maxNum.push_back(value);
+	m.lock();
+	cout << endl;
+	cout << "Max element: " << value << endl;
+	cout << "End of ";
+	cout << std::this_thread::get_id();
+	cout << " thread" << endl;
+	m.unlock();
+}
+
+
+int main()
+{
+	cout << "Input amount of elements: ";
+	size_t amount;
+	cin >> amount;
+
+	std::vector<int> vec;
+	for (size_t i = 0; i < amount; ++i) {
+		vec.push_back(std::rand());
+	}
+	cout << endl << endl;
+
+
+	int size = vec.size() / 3;
+	auto it = vec.begin();
+	size_t index1 = 0;
+	size_t index2 = size;
+	std::thread* threads[3];
+
+
+	std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
+
+	for (size_t i = 0; i < 3; ++i) {
+		if (index2 > vec.size()) {
+			index2 = vec.size();
+		}
+		threads[i] = new std::thread(findMax, vec.begin() + index1, vec.begin() + index2);
+		index1 += size;
+		index2 += size;
+	}
+	
+	for (size_t i = 0; i < 3; ++i) {
+		threads[i]->join();
+	}
+
+
+
+	std::chrono::system_clock::time_point finish = std::chrono::system_clock::now();
+	std::chrono::duration<double> def = finish - start;
+	cout << "Time used : " << def.count() << endl;
+
+
+	int max = *std::max_element(maxNum.begin(), maxNum.end());
+	cout << "MAX = " << max << endl;
+
+
+
+
+	return 0;
+}
---
+++ CMakeProject1/sem2_lab8/sem2_lab8.h
@@ -0,0 +1,14 @@
+﻿// sem2_lab8.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <random>
+
+// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
